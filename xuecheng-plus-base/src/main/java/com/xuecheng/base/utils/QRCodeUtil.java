2dbee067db943adc03c80873085dcd490f9f4812
Wwhds <a1605691832@163.com>
2024-03-06 13:03:57 +0800

    Day11完成 支付测试功能全部完成 支付依赖版本更新 订单模块测试配置内容添加

--- xuecheng-plus-base/src/main/java/com/xuecheng/base/utils/QRCodeUtil.java
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/utils/QRCodeUtil.java
@@ -76,7 +76,7 @@ public class QRCodeUtil {
 
     public static void main(String[] args) throws IOException {
         QRCodeUtil qrCodeUtil = new QRCodeUtil();
-        System.out.println(qrCodeUtil.createQRCode("http://192.168.101.1:63030/orders/alipaytest", 200, 200));
+        System.out.println(qrCodeUtil.createQRCode("http://192.168.1.8:63030/orders/alipaytest", 200, 200));
     }
 }
 
